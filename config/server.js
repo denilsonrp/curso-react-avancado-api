d740af955e7613e7dbf604abba5eecf9bfb9cd3f
Denilson <denilsonraimundo17@gmail.com>
2021-01-13 20:52:05 -0300

    :tada: Initial commit with Strapi

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '6a131e757bedc7e3be210d68195c3f16'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '088fe61c-ed8e-4876-af0d-4144e85985c2'
+};
